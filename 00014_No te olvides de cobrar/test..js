8748468307e1b6c0c9ac243311c543f64025079e
gustavo@mumuki.org <gustavo@mumuki.org>
2022-04-07 19:23:30 +0000

    Mumuki Export on 2022-04-07 19:23:30 +0000

---
+++ 00014_No te olvides de cobrar/test..js
@@ -0,0 +1,16 @@
+describe("cobrar", function() {
+  it("deberia poder cobrar un honorario", function() {
+    cobrar(["2000"])
+    assert.equal(saldoEnCuenta, 3000)
+  });
+  
+  it("deberia poder cobrar dos honorarios", function() {
+    cobrar([500, 800])
+    assert.equal(saldoEnCuenta, 2300)
+  });
+  
+  it("deberia poder cobrar tres honorarios", function() {
+    cobrar([300, 2000, 400])
+    assert.equal(saldoEnCuenta, 3700)
+  });
+})
